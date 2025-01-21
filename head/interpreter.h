8ce6d20fd186e4f35d515c9519c2e37886b62625
Ma Yuzhuo <mayuzhuonor@petalmail.com>
2025-01-21 12:03:23 +0800

    Reconstructing the interpreter and Adding a timing function.

--- head/interpreter.h
+++ head/interpreter.h
@@ -37,6 +37,10 @@ class Interpreter
 public:
     // 构造函数，接受std::vector<std::string>作为参数
     Interpreter(const std::vector<std::string> &instructions);
+    Interpreter(std::string &filename);
+    Interpreter(const Interpreter &) = delete;
+    Interpreter &operator=(const Interpreter &) = delete;
+    ~Interpreter();
 
     // 运行解释器
     void Run();
@@ -44,7 +48,12 @@ public:
     // 单步执行
     void Step();
 
+    // 获取运行时间
+    std::string getRunDuration() const;
+
 private:
+    std::string filename;
+
     std::vector<std::string>
         instructions;
     size_t currentInstruction;
@@ -53,20 +62,30 @@ private:
     // 返回当前指令的类型
     InstructionType getInstructionType(const std::string &instruction) const;
 
-    // 关键字集合
+    // 处理逻辑关键字
     static const std::unordered_set<std::string>
         logicKeywords;
+    // 处理函数关键字
     static const std::unordered_set<std::string>
         functionKeywords;
+    // 检查一个字符串是否满足C++变量的命名规则
+    bool isValidCppIdentifier(const std::string &identifier);
 
     // 变量池
-    std::unordered_map<std::string, bool>
-        variablePool;
+    std::unordered_map<std::string, bool> variablePool;
 
     // 处理未知类型的指令
-    void handleUnknownInstruction(const std::string &instruction);
+    void AddTheValueToThePool(const std::string &instruction);
 
+    // 将字符串转换为布尔值
     bool convertToBool(const std::string &str);
+
+    /// @brief  报告错误
+    void reportError(const std::string &errorMessage, int line) const;
+
+    std::chrono::high_resolution_clock::time_point start_time_;
+    std::chrono::high_resolution_clock::time_point end_time_;
+    std::string run_duration_;
 };
 
 #endif // INTERPRETER_H
